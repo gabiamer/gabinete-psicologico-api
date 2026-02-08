c68b04abae579b98b57e546eff9254d375f678de
GabMer <ana.gabrielucb.edu.bo>
2026-02-07 23:05:28 -0400

    Setup inicial: Spring Boot con YAML

---
+++ src/main/java/com/gabinete/psicologico_api/PsicologicoApiApplication.java
@@ -0,0 +1,13 @@
+package com.gabinete.psicologico_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsicologicoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsicologicoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabinete/psicologico_api/PsicologicoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabinete.psicologico_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsicologicoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
